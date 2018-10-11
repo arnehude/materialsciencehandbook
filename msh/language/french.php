c26e7ff96d3cb89a5e6017682e7e8e503dee3839
Arne Hude <Arne Hude@DESKTOP-9AQ2HP9>
2018-10-11 23:07:27 +0200

    Basic transaltion

--- msh/language/french.php
+++ msh/language/french.php
@@ -1,5 +1,11 @@
 <?php
 
+
+$fr['TRUE']                             = "Oui";
+$fr['FALSE']                            = "Non";
+$fr['SUBMIT']                           = "Soumettre";
+?><?php
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
