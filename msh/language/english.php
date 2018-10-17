beb6890da75c3f121aab06fef9d15a42968929ee
Dirk <Dirk@10.5.26.99>
2018-10-17 18:17:00 +0200

    some spelling corrections

--- msh/language/english.php
+++ msh/language/english.php
@@ -5,7 +5,7 @@ require './php/lang_requirements.php';
 
 $en['NAV_START']                        = "Navigation";
 $en['NAV_INPUT_MATERIAL']               = "Add materials (For DEV-Purposes)";
-$en['NAV_MATERIAL_LIST']                = "Materiallist (For DEV-Purposes)";
+$en['NAV_MATERIAL_LIST']                = "Material list (For DEV-Purposes)";
 $en['NAV_LIST_ALL']                     = "List all";
 $en['NAV_LIST_CATEGORY']                = "List by Category";
 $en['NAV_SINGLE_MATERIAL']              = "Navigation";
@@ -24,13 +24,13 @@ $en['MATERIAL_SIGN']                    = "How to recycle";
 $en['MATERIAL_SIGNS']                   = search_signs();
 $en['MATERIAL_PICTURES']                = search_pictures();
 
-$en['MATERIAL_ELECTRICAL_INSULATOR']    = "Value of electical insulation";
+$en['MATERIAL_ELECTRICAL_INSULATOR']    = "Value of electrical insulation";
 $en['MATERIAL_THERMAL_INSULATOR']       = "Value of thermal insulation";
-$en['MATERIAL_PHONIC_INSULATOR']        = "Is the material a phonic insulatior?";
-$en['MATERIAL_INFLAMABLE']              = "Is it inflameable?";
-$en['MATERIAL_MECHANICAL_STRESS']       = "Mecanical stress till it bend";
+$en['MATERIAL_PHONIC_INSULATOR']        = "Is the material a phonic insulator?";
+$en['MATERIAL_INFLAMABLE']              = "Is it inflammable?";
+$en['MATERIAL_MECHANICAL_STRESS']       = "Mechanical stress till it bends";
 $en['MATERIAL_ELONGATION_AT_BREAK']     = "Elongation at break";
-$en['MATERIAL_BREAKINGPOINT']           = "Mecanical stress till it break";
+$en['MATERIAL_BREAKINGPOINT']           = "Mechanical stress till it breaks";
 $en['MATERIAL_PRICE_PER_KG']            = "Price per KG";
 $en['MATERIAL_MELTINGPOINT']            = "Meltingpoint";
 $en['MATERIAL_YOUNGS_MODULE']           = "Youngs Module";
