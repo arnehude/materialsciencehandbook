28967aa3100fb970ffdb6c45b40478c00fa4a292
Dirk <Dirk@10.5.26.99>
2018-10-29 11:10:17 +0100

    first draft of about page WIP

--- msh/templates/about.php
+++ msh/templates/about.php
@@ -1,3 +1,6 @@
-hello
-
-grjuogöejdöioaehfiaehfoe
+<p>This app is primarily designed to give students, and of course others too, an easy to use platform to find detailed information about many different materials
+    presented in a clear and compact manner.</p>
+<p>It is an international student project between german technical engineering students from the University of applied science Wilhelmshaven
+    and french material engineering studends from the University of Bordeaux (IUT). </p>
+<p>Developement was assumed by Valentin Botcazou, Gaspard Meric, Arne Hude and Dirk Göpf, gathering of materials was undertaken by
+    Valentin DW, Giovanni,Théo and forgot name :) and Leadership was assumed by Arne Hude.</p>
