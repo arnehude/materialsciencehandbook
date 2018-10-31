c4881a8760f3163e9fb6cb0fcd2319e5ae922d11
arneh <arneh@DESKTOP-Arne.fritz.box>
2018-10-31 19:33:27 +0100

    Fancy new stuff (Design)

--- msh/templates/single_material.php
+++ msh/templates/single_material.php
@@ -1,8 +1,91 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-xl-8">
+                    <h1>Materialname</h1>
+                    <h4>Category</h4><strong>Topic</strong>
+                    <p>Text</p>
+                </div>
+                <div class="col-12 col-md-6 col-xl-4"><img class="rounded img-fluid d-flex" src="assets/img/bild_folgt.png" width="200px" height="200px" style="margin: 0 auto;"></div>
+            </div>
+        </div>
+    </div>
+    <div class="features-clean">
+        <div class="container">
+            <div class="row features">
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-circle icon"></i>
+                    <h3 class="name">Density</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-text-width icon"></i>
+                    <h3 class="name" style="margin: 0 0 10px;">Elongation at Break</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-thermometer-empty icon"></i>
+                    <h3 class="name">Meltingpoint</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-compress icon"></i>
+                    <h3 class="name">Mecanical Stress</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-chain-broken icon"></i>
+                    <h3 class="name">Breaking Point</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-gears icon"></i>
+                    <h3 class="name">Youngs Module</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-euro icon"></i>
+                    <h3 class="name">Price per kg</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-minus-square-o icon"></i>
+                    <h3 class="name">Insulation</h3>
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>Electric</p>
+                        </div>
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>Thermal</p>
+                        </div>
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>Phonic</p>
+                        </div>
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>Flameable</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-gears icon"></i>
+                    <h3 class="name">Youngs Module</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h4>Additional Information</h4>
+                    <p>Additional information</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
