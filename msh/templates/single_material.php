8a765cf4cf4a1fc846665e075dae57c2f37b020d
Dirk <Dirk@10.5.26.99>
2018-11-04 12:11:53 +0100

    Added material placeholders

--- msh/templates/single_material.php
+++ msh/templates/single_material.php
@@ -1,8 +1,92 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-xl-8">
+                    <h1>{{SHOW_MATERIAL_NAME}}</h1>
+                    <h4>{{SHOW_CATEGORY}}</h4><strong>{{SHOW_TOPIC}}</strong>
+                    <p>Text</p>
+                </div>
+                <div class="col-12 col-md-6 col-xl-4"><img class="rounded img-fluid d-flex" src="assets/img/bild_folgt.png" width="200px" height="200px" style="margin: 0 auto;"></div>
+            </div>
+        </div>
+    </div>
+    <div class="features-clean">
+        <div class="container">
+            <div class="row features">
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-circle icon"></i>
+                    <h3 class="name">{{SHOW_DENSITY}}</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-text-width icon"></i>
+                    <h3 class="name" style="margin: 0 0 10px;">{{SHOW_BREAK_ELONGATION}}</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-thermometer-empty icon"></i>
+                    <h3 class="name">{{SHOW_MELTINGPOINT}}</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-compress icon"></i>
+                    <h3 class="name">{{SHOW_BREAKING_POINT}}</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-chain-broken icon"></i>
+                    <h3 class="name">{{SHOW_YOUNGS_MODULE}}</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-gears icon"></i>
+                    <h3 class="name">{{SHOW_PPK}}</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-minus-square-o icon"></i>
+                    <h3 class="name">{{SHOW_INSULATION}}</h3>
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>{{SHOW_INSULATION_ELECTRIC}}</p>
+                        </div>
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>{{SHOW_INSULATION_THERMAL}}</p>
+                        </div>
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>{{SHOW_INSULATION_PHONIC}}</p>
+                        </div>
+                        <div class="col" style="padding: 5px;"><span class="badge badge-primary" style="font-size: 1em;margin: 0 auto;">YES</span>
+                            <p>{{SHIW_FLAMMABILITY}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-euro icon"></i>
+                    <h3 class="name">currently unused</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-lg-4 item"><i class="fa fa-gears icon"></i>
+                    <h3 class="name">currently unused</h3>
+                    <div class="progress">
+                        <div class="progress-bar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"><span class="sr-only">50%</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h4>{{SHOW_ADDITIONAL_INFORMATION}}</h4>
+                    <p>Additional information</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
